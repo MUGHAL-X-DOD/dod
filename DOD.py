875ec47aee382056a754ccc55383cb7d2d233d55
MUHAMMAD HAMMAD <138203938+MUGHAL-X-DOD@users.noreply.github.com>
2023-11-29 09:54:52 +0500

    Update DOD.py

--- DOD.py
+++ DOD.py
@@ -1,8 +1,8 @@
 import os,platform
 os.system('git pull')
 
-trt=platform.architecture()[0]
-if trt=="32bit":
+dod=platform.architecture()[0]
+if dod=="32bit":
     print('Sorry 32 Bit Not Saported...')
-elif trt=="64bit":
+elif dod=="64bit":
     __import__("xd")
